46e5593eb4f13815a04420e5ed850ecd08192d4d
Richie Bendall <richiebendall@gmail.com>
2020-10-30 16:38:25 +1300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 // Adapted from: https://github.com/IonicaBizau/regex-parser.js/blob/a5746771c0ee2e1a3ab1557905ef734c047da0d5/lib/index.js
 
 module.exports = (regex) => {
+	if (regex instanceof Regexp) {
+		return {
+			main: regex.source,
+			options: regex.flags
+		}
+	}
+	
 	const matches = regex.toString().match(/(\/?)(?<main>.+)\1(?<options>[a-z]*)/i)
 
 	if (matches === null) throw new SyntaxError("Unable to parse regex")
